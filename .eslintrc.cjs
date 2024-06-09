60f99cb20624afbb93fb44bc38b2498279417163
Joncarlos Perdomo <joncarlos.perdomo@whatifmediagroup.com>
2024-06-08 23:56:15 -0400

    chore: Update vite config to resolve aliases for components and utils

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
@@ -14,10 +19,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
   },
