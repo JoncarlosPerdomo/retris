2be7ebd1118fe59dab75d81ac9c586ba82aad2cb
Joncarlos Perdomo <joncarlos.perdomo@whatifmediagroup.com>
2024-06-08 20:02:24 -0400

    update base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'http://z8g0wsc.5.78.72.145.sslip.io/',
+});
